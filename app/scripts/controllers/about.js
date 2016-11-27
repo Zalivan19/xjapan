25ca135e9d43696ddc8a75777e1f7a03966eddbf
Zalivan19 <tantan19957@gmail.com>
2016-11-27 18:31:15 -0500

    primer commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xjapanApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the xjapanApp
+ */
+angular.module('xjapanApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/inicio.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xjapanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the xjapanApp
+ */
+angular.module('xjapanApp')
+  .controller('InicioCtrl', function () {
+
+  });
